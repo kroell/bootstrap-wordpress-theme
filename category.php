b560573a33b06b7ff535f37a05e8f57bb1fa801f
Soeren Kroell <hallo@soerenkroell.com>
2015-07-17 14:45:45 +0200

    Wieder alles da

--- category.php
+++ category.php
@@ -3,8 +3,15 @@
 <?php get_header(); ?>
 
 	<?php while ( have_posts() ) : the_post(); ?>
+ 
+		<article class="category-item">
+			<h1><?php the_title(); ?></h1>
+			<p>Geschrieben von: <?php the_author(); ?></p>
 
-		
+			<?php the_excerpt(); ?>
+
+			<a href="<?php the_permalink(); ?>" class="btn btn-primary">weiterlesen</a>
+		</article>
 
 	<?php endwhile; ?>
 
