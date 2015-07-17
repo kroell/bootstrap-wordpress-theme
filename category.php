4f0a9b125368efa062faabd252b3f8ba9aa8578f
Soeren Kroell <hallo@soerenkroell.com>
2015-07-17 14:41:52 +0200

    alles weg

--- category.php
+++ category.php
@@ -4,14 +4,7 @@
 
 	<?php while ( have_posts() ) : the_post(); ?>
 
-		<article class="category-item">
-			<h1><?php the_title(); ?></h1>
 		
-
-			<?php the_excerpt(); ?>
-
-			<a href="<?php the_permalink(); ?>" class="btn btn-primary">weiterlesen</a>
-		</article>
 
 	<?php endwhile; ?>
 
